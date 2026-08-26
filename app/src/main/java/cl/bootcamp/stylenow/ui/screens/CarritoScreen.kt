7c6de4850799688b79794630883d0e5a622b091e
Maximiliano <maxyrojas.al@gmail.com>
2026-08-25 23:45:17 -0400

    chore: eliminar imports innecesarios

--- app/src/main/java/cl/bootcamp/stylenow/ui/screens/CarritoScreen.kt
+++ app/src/main/java/cl/bootcamp/stylenow/ui/screens/CarritoScreen.kt
@@ -1,23 +1,13 @@
 package cl.bootcamp.stylenow.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,12 +15,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cl.bootcamp.stylenow.data.DefaultData
 import cl.bootcamp.stylenow.ui.components.ItemProductoCarrito
-import kotlin.collections.remove
 
 @Composable
 fun CarritoScreen(
